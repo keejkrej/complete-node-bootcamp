bc75223c9746af04b14a1f9e1ee09669fff55b26
ctyjackcao <ctyjackcao@gmail.com>
2025-06-08 17:20:36 +0000

    4

---
+++ 4-natours/starter/controllers/tourController.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+const checkID = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+};
+
+const checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price'
+        });
+    }
+    next();
+};
+
+const getAllTours = (req, res) => {
+    console.log(req.requestTime);
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+};
+
+const getTour = (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    });
+};
+
+const createTour = (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = {
+        id: newId,
+        ...req.body
+    };
+    tours.push(newTour);
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            });
+        }
+    );
+};
+
+const updateTour = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: req.body
+        }
+    });
+};
+
+const deleteTour = (req, res) => {
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+};
+
+module.exports = {
+    checkID,
+    checkBody,
+    getAllTours,
+    getTour,
+    createTour,
+    updateTour,
+    deleteTour,
+};
---
+++ 4-natours/starter/controllers/userController.js
@@ -0,0 +1,42 @@
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: ''
+    })
+}
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: ''
+    })
+}
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: ''
+    })
+}
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: ''
+    })
+}
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: ''
+    })
+}
+
+module.exports = {
+    getAllUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+}
